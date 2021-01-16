1559a3f0adddf84fd31a9f47fb42baa834f57085
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-01-16 18:37:44 +0500

    MHS Permalinks Reference.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import React,{useEffect} from 'react'
 import './App.css';
 import {Switch,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home';
-import Project2 from './Pages/Project2/Project2';
-import Project3 from './Pages/Project3/Project3';
-import Project4 from './Pages/Project4/Project4';
-import Project5 from './Pages/Project5/Project5';
 function App() {
   return (
     <div className="App">
-      {/* <Switch> */}
-        <Route path="/" component={Home}/>
-        <Route path="/project2" component={Project2}/>
-        <Route path="/project3" component={Project3}/>
-        <Route path="/project4" component={Project4}/>
-        <Route path="/project5" component={Project5}/>
-      {/* </Switch> */}
+        <Route path="/" component={Home} />
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect } from "react";
+import "./Home.css";
+import ScrollableSection, { ScrollableLink } from "react-update-url-on-scroll";
 function Home() {
-    return (
-        <div className="home-wrapper-container">
-            <div className="home-container">
-                <h1>Project1</h1>
-            </div>
+  return (
+    <div className="home-wrapper-container">
+ 
+      <ScrollableSection name={"project1"}>
+        <div className="home-container">
+          <h1>Project1-Home</h1>
         </div>
-    )
+      </ScrollableSection>
+      <ScrollableSection name={"project2"}>
+        <div className="home-container2">
+          <h1>Project2-Home</h1>
+        </div>
+      </ScrollableSection>
+      <ScrollableSection name={"project3"}>
+        <div className="home-container3">
+          <h1>Project3-Home</h1>
+        </div>
+      </ScrollableSection>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
