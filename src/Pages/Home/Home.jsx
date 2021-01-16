e0b864b39420c3b9e887ec195d0cab1cb6f9f124
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-01-17 01:13:12 +0500

    MHS Basic scroll structure.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,63 @@ import ScrollableSection, { ScrollableLink } from "react-update-url-on-scroll";
 function Home() {
   return (
     <div className="home-wrapper-container">
- 
       <ScrollableSection name={"project1"}>
         <div className="home-container">
-          <h1>Project1-Home</h1>
+          <div className="header bg-silver" style={{zIndex:"2"}}></div>
+          <div className="left-nav">
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+            <h1>NEW</h1>
+          </div>
+          <div className="projectsContent">
+            <h1 className="test" style={{zIndex:"3"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+            <h1 style={{zIndex:"1"}}>Content</h1>
+          </div>
         </div>
       </ScrollableSection>
-      <ScrollableSection name={"project2"}>
+      {/* <ScrollableSection name={"project2"}>
         <div className="home-container2">
           <h1>Project2-Home</h1>
         </div>
@@ -20,6 +70,7 @@ function Home() {
           <h1>Project3-Home</h1>
         </div>
       </ScrollableSection>
+     */}
     </div>
   );
 }
