5e3d39c0635588e74d95eafb97efd47191c02b55
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-01-20 14:37:57 +0500

    MHS 1.0.16 Edit 1 done.

--- src/Components/NavItem/NavItem.jsx
+++ src/Components/NavItem/NavItem.jsx
@@ -3,30 +3,29 @@ import { withRouter } from "react-router-dom";
 import ScrollableSection, { ScrollableLink } from "react-update-url-on-scroll";
 import "./NavItem.css";
 function NavItem(props) {
-  const [itemColor, setitemColor] = useState('lightgrey')
+  const [itemColor, setitemColor] = useState("lightgrey");
   useEffect(() => {
     if (props.currentUrl === `/${props.project.title[0].text}`)
-      setitemColor('blue')
-    else
-      setitemColor('lightgrey')
-    return () => {
-    }
-  }, [props.currentUrl])
+      setitemColor("blue");
+    else setitemColor("lightgrey");
+    return () => {};
+  }, [props.currentUrl]);
   return (
     <ScrollableLink href={`/${props.project.title[0].text}`}>
       <div
         className="nav-item-container-wrapper flex-col pointer"
         style={{ color: `${itemColor}` }}
       >
-        <h2 className="f1-5">{props.project.title[0].text}</h2>
-        <p className="f1-3">
+        <h2 className="f1-5 item-heading">
+          <span style={{ flex: "3" }}>{props.project.title[0].text}</span>
+          <span style={{ flex: "1" }}>{props.project.projectdates}</span>
+        </h2>
+        <p className="f1-3 item-para">
           The garage opened in March 2018 . I managed to get paid in August.
           People repair their vehicles themselves and I accompany them. It is
           not...
         </p>
-        <h2 className="item-date">
-          {props.project.date}
-        </h2>
+        {/* <h2 className="item-date f1-5 fb">{props.project.projectdates}</h2> */}
       </div>
     </ScrollableLink>
   );
