7bd92223ee7b803f31b8011ea6b364b3e4398c78
Osama Azhar <osamaazhar.edu@gmail.com>
2021-01-20 15:44:25 +0500

    MHS 1.0.19 AboutRoute Added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 function App() {
   return (
     <div className="App">
+      <Switch>
         <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        </Switch>
     </div>
   );
 }
