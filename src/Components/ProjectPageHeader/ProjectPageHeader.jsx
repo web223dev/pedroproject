dca684731872e8d3bd0d526488432b9dbcb5d7ce
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-02-14 02:14:45 +0500

    MHS 1.1.18

--- src/Components/ProjectPageHeader/ProjectPageHeader.jsx
+++ src/Components/ProjectPageHeader/ProjectPageHeader.jsx
@@ -5,7 +5,7 @@ function ProjectPageHeader(props) {
     return (
         <div className="projectpage-header-container-wrapper flex-col-end">
             <div className="projectpage-header-container">
-                <div className="projectpage-name fb pointer" onClick={() => { props.history.push('/') }}><i className="fas fa-arrow-left"></i> Pedro Damasceno</div>
+                <div className="projectpage-name fb pointer" onClick={() => { props.history.push('/') }}><i className="f1-3 fas fa-arrow-left"></i> Pedro Damasceno</div>
                 {/* <div className="projectpage-initiative">Initiative</div> */}
                 <Link to="/about">
                     <div className="projectpage-about">About</div>
